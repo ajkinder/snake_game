25090fbb20943a7e11aff0752b1444765dbad47e
Alexander Kinder <ajkinder@ucsc.edu>
2021-05-15 19:01:36 -0700

    Add functionality to allow user to change the direction of the snake using arrow keys.

--- main.py
+++ main.py
@@ -14,12 +14,19 @@ screen.title("Crazy Snake")  # Set Screen Title
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
 game_is_on = True
 while (game_is_on):
     screen.update()
     time.sleep(0.1)
 
     snake.move()
+
 
 
 
